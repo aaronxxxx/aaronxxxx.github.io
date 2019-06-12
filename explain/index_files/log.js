4ce719041f13f4e52d30d9deb5ac9b6b7424cbdc
feixu <feixu.designer@gmail.com>
2019-06-12 17:52:23 +0800

    ADD

--- explain/index_files/log.js
+++ explain/index_files/log.js
@@ -7,12 +7,12 @@ window.onload = function () {
             overlayClose: true,
             content: 'QQ号复制成功，立刻为你打开QQ',
             onClickConfirmBtn: function () {
-                window.location.href = '"http://wpa.qq.com/msgrd?v=3&uin=1685947711&site=qq&menu=yes';
+                window.location.href = 'http://wpa.qq.com/msgrd?v=3&uin=1685947711&site=qq&menu=yes';
             }
         });
 
         setTimeout(function () {
-            window.location.href = '"http://wpa.qq.com/msgrd?v=3&uin=1685947711&site=qq&menu=yes';
+            window.location.href = 'http://wpa.qq.com/msgrd?v=3&uin=1685947711&site=qq&menu=yes';
         }, 3000);
 
         e.clearSelection();
@@ -20,7 +20,7 @@ window.onload = function () {
 
     clipboard.on('error', function (e) {
         setTimeout(function () {
-            window.location.href = '"http://wpa.qq.com/msgrd?v=3&uin=1685947711&site=qq&menu=yes';
+            window.location.href = 'http://wpa.qq.com/msgrd?v=3&uin=1685947711&site=qq&menu=yes';
         }, 3000);
         var dialog1 = $(document).dialog({
             type: 'confirm',
@@ -40,7 +40,7 @@ window.onload = function () {
             overlayClose: true,
             content: '<div><p>恭喜获得客服指导一次，点击添加客服微信</p><p style="font-size:22px;text-align: center;color: red; ">立刻添加微信</p></div>',
             onClickConfirmBtn: function () {
-                window.location.href = '"http://wpa.qq.com/msgrd?v=3&uin=1685947711&site=qq&menu=yes';
+                window.location.href = 'http://wpa.qq.com/msgrd?v=3&uin=1685947711&site=qq&menu=yes';
              // layer.closeAll();
             }
         });
